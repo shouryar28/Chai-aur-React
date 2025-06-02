a648868cb6000f986ce22a69ad547934482dfaad
shouryar28 <shouryar046@gmail.com>
2025-06-02 18:55:10 +0530

    Add initial code

---
+++ Basic_React_Vite/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./Chai"
+function App() {
+
+  return (
+    <>
+    <h1>Basic of react | Shourya Raj</h1>
+    <Chai />
+    </>
+    
+  )
+}
+
+export default App
---
+++ Basic_React_Vite/src/Chai.jsx
@@ -0,0 +1,6 @@
+function Chai() {
+    return (
+        <h1>Chai ko import kiya jaa rha hai</h1>
+    )
+}
+export default Chai
